976fde0f2d7e258b6d312c52695e7dc223404215
novucs <contact@novucs.net>
2018-02-21 18:09:47 +0000

    Process bytes on receiving

--- source/main.cpp
+++ source/main.cpp
@@ -8,8 +8,10 @@
 #define TICK_RATE 75
 
 enum State {
+    RESET,
     FIRST,
-    PIPE
+    READ_LENGTH,
+    READ_PAYLOAD
 };
 
 State state = FIRST;
@@ -17,10 +19,53 @@ MicroBit uBit;
 std::queue<char> bytes;
 char bits = 0;
 int bits_size = 0;
+int packet_size = 0;
+int read_size = 0;
+
+std::vector<char> read_all(int length);
+
+void on_byte_received(char byte) {
+    bits = 0;
+    bits_size = 0;
+    bytes.push(byte);
+
+    static char last_byte = 0;
+    switch (state) {
+        case READ_LENGTH: {
+            read_size += 1;
+
+            if (read_size == 2) {
+                read_size = 0;
+                packet_size = (last_byte << 4) + byte;
+                uBit.display.print(packet_size);
+                state = READ_PAYLOAD;
+            }
+
+            last_byte = byte;
+            break;
+        }
+        case READ_PAYLOAD: {
+            read_size += 1;
+            if (byte == 104) {
+                uBit.display.print("A");
+            }
+
+            if (byte == 105) {
+                uBit.display.print("B");
+            }
+
+            if (read_size == packet_size) {
+                read_size = 0;
+                state = READ_LENGTH;
+            }
+            break;
+        }
+    }
+}
 
 void on_hi(MicroBitEvent event) {
     if (state == FIRST) {
-        state = PIPE;
+        state = READ_LENGTH;
         return;
     }
 
@@ -32,14 +77,16 @@ void on_hi(MicroBitEvent event) {
         bits_size += 1;
 
         if (bits_size == 8) {
-            bytes.push(bits);
-            bits = 0;
-            bits_size = 0;
+            on_byte_received(bits);
         }
     }
 }
 
 void on_lo(MicroBitEvent event) {
+    if (state == FIRST) {
+        return;
+    }
+
     uint64_t ticks = (event.timestamp / 1000) / TICK_RATE;
 
     for (int i = 0; i < ticks; i++) {
@@ -47,15 +94,13 @@ void on_lo(MicroBitEvent event) {
         bits_size += 1;
 
         if (bits_size == 8) {
-            bytes.push(bits);
-            bits = 0;
-            bits_size = 0;
+            on_byte_received(bits);
         }
     }
 }
 
 std::vector<char> read_all(int length) {
-    vector<char> target;
+    std::vector<char> target;
 
     while (true) {
         while (bytes.size() > 0 && length > 0) {
@@ -88,9 +133,9 @@ void read() {
     uBit.messageBus.listen(MICROBIT_ID_IO_P2, MICROBIT_PIN_EVT_PULSE_LO, on_lo);
 
     while (true) {
-        short packet_length = read_short();
-        std::vector<char> payload = read_all(packet_length);
-        uBit.display.scroll(ManagedString(payload.data(), (uint16_t)payload.size()));
+//        short packet_length = read_short();
+//        std::vector<char> payload = read_all(packet_length);
+//        uBit.display.scroll(ManagedString(payload.data(), (uint16_t)payload.size()));
         uBit.sleep(TICK_RATE);
     }
 }
@@ -112,12 +157,12 @@ void write_byte(char byte) {
 }
 
 void write_short(short value) {
-    write_byte((char)(value >> 8));
-    write_byte((char)(value & 0xFF));
+    write_byte((char) (value >> 8));
+    write_byte((char) (value & 0xFF));
 }
 
 void write_string(const std::string &message) {
-    write_short((short)message.size());
+    write_short((short) message.size());
     for (char character : message) {
         write_byte(character);
     }
@@ -130,18 +175,22 @@ void write() {
 
     uBit.sleep(500);
     uBit.display.print("b");
-    uBit.io.P1.setDigitalValue(1);
-    uBit.sleep(TICK_RATE);
+    write_bit(true);
 
     while (true) {
-        write_string("hello friend. :)");
+//        write_string("hello friend. :)");
+        write_short(2);
+        write_byte(104);
+        write_byte(105);
+//        write_bit(true);
+//        write_bit(false);
     }
 }
 
 int main() {
     uBit.init();
-//    read();
-    write();
+    read();
+//    write();
     return EXIT_SUCCESS;
 }
 
